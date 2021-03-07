3a6b172bd9a1df207e543827e1b9ce049911a4d4
sheilz <48970968+sheesh19@users.noreply.github.com>
2021-03-07 12:01:33 -0500

    Update orders_view.rb

--- oop/food-delivery/day-two/01-Food-Delivery/app/views/orders_view.rb
+++ oop/food-delivery/day-two/01-Food-Delivery/app/views/orders_view.rb
@@ -3,9 +3,15 @@ require_relative 'base_view'
 class OrdersView < BaseView
 
   def display(orders)
-    orders.each do |order|
-      puts "#{order.id} - #{order.customer.name} #{order.meal.name}"
+    orders.each_with_index do |order, index|
+      puts "#{index + 1}. #{order.employee.username} must deliver #{order.meal.name} to #{order.customer.name}"
     end
   end
+
+#  def display(orders)
+#    orders.each do |order|
+#      puts "#{order.id} - #{order.customer.name} #{order.meal.name}"
+#    end
+#  end
 end
 
