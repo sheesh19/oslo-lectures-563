1ba3997cc6b4618c3cb08665ef0759492698d3ef
sheilz <48970968+sheesh19@users.noreply.github.com>
2021-03-31 22:08:34 -0400

    Update room_repository.rb

--- oop/food-delivery/day-one/repositories/room_repository.rb
+++ oop/food-delivery/day-one/repositories/room_repository.rb
@@ -1,27 +1,36 @@
-require "csv"
-require_relative "../models/room"
+require 'csv'
+require_relative '../models/room'
 
 class RoomRepository
+  attr_reader :rooms
+
   def initialize(csv_path)
     @csv_path = csv_path
+    @csv_options = { headers: :first_row, header_converters: :symbol }
     @rooms = []
     load_csv
   end
 
   def find(id)
-    @rooms.find { |room| room.id = id }
+    @rooms.find { |room| room.id == id }
   end
 
   private
 
   def load_csv
-    csv_options = { headers: true, header_converters: :symbol }
-    CSV.foreach(@csv_path, csv_options) do |row|
+    CSV.foreach(@csv_path, @csv_options) do |row|
       # Typecasting -> updating our data types
       # id:integer, number:string
       row[:id] = row[:id].to_i
+      row[:capacity] = row[:capacity].to_i
 
       @rooms << Room.new(row)
     end
   end
 end
+
+
+# room_csv_file = File.join(__dir__, '../data/rooms.csv')
+# room_repo = RoomRepository.new(room_csv_file)
+
+# p room_repo.rooms
